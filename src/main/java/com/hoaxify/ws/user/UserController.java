8100a782419741707c1fa8e6609cc0d870e953b5
mbayindirli <mustafabayindirli27@gmail.com>
2020-12-27 18:28:01 +0300

    Http Response cevaplarını değiştirmek

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -5,8 +5,10 @@ package com.hoaxify.ws.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,6 +23,7 @@ public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/api/1.0/users")
+    @ResponseStatus(HttpStatus.CREATED)
     public void createUser(@RequestBody User  user){
        userService.cretaUser(user);
     }
