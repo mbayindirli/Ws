3f9615c0d3670da906bc9f9fd665381c9a3e8a30
mbayindirli <mustafabayindirli27@gmail.com>
2020-12-27 14:52:05 +0300

    What is Lombok?

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -1,40 +1,12 @@
 package com.hoaxify.ws.user;
 
+import lombok.Data;
+
+@Data
+//lombok Kütüphanesini ekledik ve Lombook ile glen @Data ntasyonu sayesinde getter setter toSAtrig metotlarını generate etmeye gerek kalmadı
 public class User {
     private String username;
     private String displayName;
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", displayName='" + displayName + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
