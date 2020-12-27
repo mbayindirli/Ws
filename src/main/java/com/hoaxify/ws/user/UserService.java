c0ea8295e43f9ee988a8741eff76e0940d938372
mbayindirli <mustafabayindirli27@gmail.com>
2020-12-27 18:09:53 +0300

    Dependancy Cunstroctor sayesinde gerçeklemek

--- src/main/java/com/hoaxify/ws/user/UserService.java
+++ src/main/java/com/hoaxify/ws/user/UserService.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Service;
 
 @Service//sadece okunurluğu artırmak için
 public class UserService {
-    @Autowired
     UserRepository userRepository;
+    @Autowired//constructor ile dependancy injection yapmak buada injection yapılan class bir tane olduğundan @Autowired zorunl değil dazha fazla olsaydı zorunlu olurdu
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public void cretaUser(User user){
         userRepository.save(user);
     }
