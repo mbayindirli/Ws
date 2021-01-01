4bb66c08c8dcd828bed16271e6ee5b82d51699eb
mbayindirli <mustafabayindirli27@gmail.com>
2021-01-01 22:34:21 +0300

    Uniqe username database seviyesinde

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -3,10 +3,13 @@ package com.hoaxify.ws.user;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -18,10 +21,17 @@ public class User {
     private Long id;
     
     @NotNull
+    @Size(min = 3,max = 255)
+    @Column(unique = true)
     private String userName;
 
     @NotNull
+    @Size(min = 3,max = 255)
     private String displayName;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\\d]){1,})(?=(.*[\\W]){1,})(?!.*\\s).{8,}$", flags = Pattern.Flag.UNICODE_CASE)
+    @Size(min = 8,max = 255)
     private String password;
 
 
